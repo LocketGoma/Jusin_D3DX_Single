f74c0dfa86632ec6e8ef4725b0c8a5aca3fdc8a4
LocketGoma <nothappa@naver.com>
2021-03-03 13:06:15 +0900

    그냥 MFC 툴 만들기로...

---
+++ D3DX_Single_GameProject/D3DX_Single_Tools/D3DX_Single_ToolsDoc.cpp
@@ -0,0 +1,138 @@
+﻿
+// D3DX_Single_ToolsDoc.cpp: CD3DXSingleToolsDoc 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "D3DX_Single_Tools.h"
+#endif
+
+#include "D3DX_Single_ToolsDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CD3DXSingleToolsDoc
+
+IMPLEMENT_DYNCREATE(CD3DXSingleToolsDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CD3DXSingleToolsDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CD3DXSingleToolsDoc 생성/소멸
+
+CD3DXSingleToolsDoc::CD3DXSingleToolsDoc() noexcept
+{
+	// TODO: 여기에 일회성 생성 코드를 추가합니다.
+
+}
+
+CD3DXSingleToolsDoc::~CD3DXSingleToolsDoc()
+{
+}
+
+BOOL CD3DXSingleToolsDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 여기에 재초기화 코드를 추가합니다.
+	// SDI 문서는 이 문서를 다시 사용합니다.
+
+	return TRUE;
+}
+
+
+
+
+// CD3DXSingleToolsDoc serialization
+
+void CD3DXSingleToolsDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 여기에 저장 코드를 추가합니다.
+	}
+	else
+	{
+		// TODO: 여기에 로딩 코드를 추가합니다.
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 축소판 그림을 지원합니다.
+void CD3DXSingleToolsDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 문서의 데이터를 그리려면 이 코드를 수정하십시오.
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 검색 처리기를 지원합니다.
+void CD3DXSingleToolsDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 문서의 데이터에서 검색 콘텐츠를 설정합니다.
+	// 콘텐츠 부분은 ";"로 구분되어야 합니다.
+
+	// 예: strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CD3DXSingleToolsDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CD3DXSingleToolsDoc 진단
+
+#ifdef _DEBUG
+void CD3DXSingleToolsDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CD3DXSingleToolsDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CD3DXSingleToolsDoc 명령
---
+++ D3DX_Single_GameProject/D3DX_Single_Tools/D3DX_Single_ToolsView.h
@@ -0,0 +1,49 @@
+﻿
+// D3DX_Single_ToolsView.h: CD3DXSingleToolsView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CD3DXSingleToolsView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CD3DXSingleToolsView() noexcept;
+	DECLARE_DYNCREATE(CD3DXSingleToolsView)
+
+// 특성입니다.
+public:
+	CD3DXSingleToolsDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CD3DXSingleToolsView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // D3DX_Single_ToolsView.cpp의 디버그 버전
+inline CD3DXSingleToolsDoc* CD3DXSingleToolsView::GetDocument() const
+   { return reinterpret_cast<CD3DXSingleToolsDoc*>(m_pDocument); }
+#endif
+
